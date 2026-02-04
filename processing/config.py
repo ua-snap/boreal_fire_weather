da16e0dc7d06bbbef7d44608296e7fbcac726900
joshdpaul <jdpaul3@alaska.edu>
2026-02-04 08:50:25 -0900

    start config

---
+++ processing/config.py
@@ -0,0 +1,12 @@
+import os
+
+DATA_DIR = os.getenv("DATA_DIR")
+OUT_DIR = os.getenv("OUT_DIR")
+
+source_zips = [
+    "CNRM-CM6-1-HR.zip",
+    "EC-Earth3-Veg.zip",
+    "era5.zip",
+    "MPI-ESM1-2-HR.zip",
+    "MRI-ESM2-0.zip",
+]
